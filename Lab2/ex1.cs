c315cc7ee034dddf79c2e9b40ac29ea2334c1802
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-09-24 23:20:01 +0900

    .

---
+++ Lab2/ex1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic; //to use list 
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Random ran = new System.Random();
+
+
+            List<int> list = new List<int>();
+
+
+            //generate random number
+            Console.WriteLine(ran.Next(2, 51));
+
+
+           
+
+
+        }
+    }
+}
